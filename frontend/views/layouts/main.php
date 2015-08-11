f7fb8b46282405e90390ce2e39669ff5d32400fd
xinxin <xinxin@rrkd.cn>
2015-08-11 14:17:10 +0800

    for yii2test:1,delete some useless files. 2,modify name of the nav to cn

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -35,15 +35,15 @@ AppAsset::register($this);
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => '主页', 'url' => ['/site/index']],
+        ['label' => '关于', 'url' => ['/site/about']],
+        ['label' => '联系', 'url' => ['/site/contact']],
         ['label' => 'Entry', 'url' => ['/site/entry']],
-        ['label' => 'Country', 'url' => ['/country/index']],
+        ['label' => '国家', 'url' => ['/country/index']],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        $menuItems[] = ['label' => '注册', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => '登录', 'url' => ['/site/login']];
     } else {
         $menuItems[] = [
             'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
